a689cf7b7c34bff84600550cf545be0c603db272
rtaukir26 <111485623+rtaukir26@users.noreply.github.com>
2023-08-14 12:58:16 +0530

    side menu layout done

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,8 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
-// import App from "../Terminal/Terminal";
-
+import EidIcon1 from "../../assets/images/eid1.avif";
+import EidIcon2 from "../../assets/images/eid2.avif";
+import EidIcon3 from "../../assets/images/eid3.jpg";
+import EidIcon4 from "../../assets/images/eid4.jpg";
+import DiwaliIcon1 from "../../assets/images/diwali1.avif";
+import DiwaliIcon2 from "../../assets/images/diwali2.avif";
+import DiwaliIcon3 from "../../assets/images/diwal3.avif";
+import DiwaliIcon4 from "../../assets/images/diwali4.avif";
+import DiwaliIcon5 from "../../assets/images/diwali5.avif";
+export const bannerEidImg = [
+  { imgName: EidIcon1 },
+  { imgName: EidIcon2 },
+  { imgName: EidIcon3 },
+  { imgName: EidIcon4 },
+];
+export const bannerDiwaliImg = [
+  { imgName: DiwaliIcon1 },
+  { imgName: DiwaliIcon2 },
+  { imgName: DiwaliIcon3 },
+  { imgName: DiwaliIcon4 },
+  // { imgName: DiwaliIcon5 },
+];
 const Home = () => {
+  const [image, setImage] = useState([]);
+  const [dynamicImgClass, setDynamicImgClass] = useState("banner_img1");
+  // const [source, setSource] = useState({ videoUrl: [], imgUrl: [] });
+  const [source, setSource] = useState([]);
+  const [imgUrl, setImgUrl] = useState([]);
+  //imges changes dynamacally, loader
+  const callAfterdelay = (i) => {
+    setTimeout(() => {
+      for (let i = 0; i <= bannerEidImg.length; i++) {
+        seqOfIndex(i);
+      }
+    }, 10000 * i);
+  };
+  const seqOfIndex = (index) => {
+    setTimeout(() => {
+      setTimeout(() => {
+        if (index < bannerEidImg.length) {
+          setImage(bannerEidImg[index]);
+          setDynamicImgClass("banner_img1");
+          console.log(index, dynamicImgClass);
+          setTimeout(() => {
+            setDynamicImgClass("");
+          }, 1500);
+        } else {
+          callAfterdelay(0);
+        }
+      }, 2000);
+    }, 8000 * index);
+  };
+  useEffect(() => {
+    callAfterdelay();
+  }, []);
+  console.log("dd source", source);
+  //file uplaod
+  const handleChangeFile = (e) => {
+    let files = e.target.files[0];
+    const url = URL.createObjectURL(files);
+    setImgUrl((pre) => [...pre, url]);
+    // setSource((pre) => {
+    //   return { ...pre,...pre.imgUrl,[pre.imgUrl, url] };
+    // });
+    // let reader = new FileReader();
+    // reader.readAsDataURL(files);
+    // reader.onload = (e) => {
+    //   // this.setstate{image:(e.target.result)}
+    //   setSource(e.target.result);
+    //   /*
+    // const url="http://localhost:3000/api/service";
+    // const formData={file:e.target.result}
+    // return post(url,formData)
+    // .then(response=>console.log(response));
+    // */
+    // };
+  };
+  //video upalod
+  const handleVideoChange = (event) => {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    // setSource((pre) => [...pre, [...videoUrl, url]]);
+    setSource((pre) => [...pre, url]);
+  };
+
   return (
     <section className="super_main_sec">
       <Header />
@@ -14,6 +96,7 @@ const Home = () => {
               <input type="text" placeholder="search by festival name" />
             </div>
             <ul>
+              <li>All</li>
               <li>Eid</li>
               <li>Diwali</li>
               <li>Bakrid</li>
@@ -30,11 +113,76 @@ const Home = () => {
           {/* ===right side body=== */}
           <div className="right_div">
             <div className="banner_div">
-              <div className="banner1">banner1</div>
+              <div className={`banner1 ${dynamicImgClass}`}>
+                <img
+                  // className="banner_img1"
+                  src={
+                    image?.imgName ? image?.imgName : bannerEidImg[0]?.imgName
+                  }
+                  alt="b1"
+                />
+              </div>
               <div className="banner1">2</div>
               <div className="banner1">3</div>
             </div>
-            <div className="right_body_div"></div>
+            <div className="right_body_div">
+              <div className="upload_div">
+                <div>
+                  {/* <input
+                    className="custom-file-input"
+                    type="file"
+                    onChange={handleChangeFile}
+                  /> */}
+                  <input
+                    type="file"
+                    onChange={handleChangeFile}
+                    id="file-input"
+                    name="file-input"
+                  />
+
+                  <label id="file-input-label" for="file-input">
+                    Select an Image
+                  </label>
+                  {/* {imgUrl && <img src={imgUrl} alt="img" />} */}
+                  <input
+                    // ref={inputRef}
+                    className="VideoInput_input"
+                    type="file"
+                    onChange={handleVideoChange}
+                    accept=".mov,.mp4"
+                  />
+                  {/* {source && (
+                    <video
+                      className="VideoInput_video"
+                      width="100%"
+                      height="400px"
+                      controls
+                      src={source}
+                    />
+                  )} */}
+                </div>
+                {/* {source.length > 0 ? (
+                  <div className="post_div">
+                    {source.map((item) => (
+                      <div className="post_sub_div">
+                        <div className="img_div">
+                          <img src={source?.imgUrl} alt="img" />
+                        </div>
+                        <div className="video_div">
+                          <video
+                            className="VideoInput_video"
+                            width="100%"
+                            height="400px"
+                            controls
+                            src={source?.videoUrl}
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                ) : null} */}
+              </div>
+            </div>
           </div>
         </div>
       </div>
